43dccb27197cf73dacdab5c5cc59320629e84265
Saptarshi Pujari <141497431+saptarshi103@users.noreply.github.com>
2024-04-12 21:31:53 +0530

    Add files via upload

--- Inheritance.cpp
+++ Inheritance.cpp
@@ -1,59 +1,76 @@
-#include <iostream>
-#include <cmath>
-
+#include<iostream>
 using namespace std;
 
-class Shape {
+class Father{
 public:
-    virtual double area() const = 0;
-    virtual double perimeter() const = 0;
+    int f_p , t_f_p , pf;
+    void get_Datafather(){
+        cout<<"Enter the total  Father's Property : "<<endl;
+        cin>>t_f_p;
+        
+    }
+    void DisplayFather(){
+        cout<<"The Total Father's property is :" <<t_f_p<<endl;
+    }
+    void passmypropertyf(){
+        char choice;
+        cout << "Do you want to pass the property to son ? (y/n): ";
+        cin >> choice;
+        if(choice == 'y' || choice == 'Y')
+            pf = t_f_p;
+        else
+            pf = 0;
+    }
+
 };
 
-class Rectangle : public Shape {
-private:
-    double length;
-    double width;
-
-public:
-    Rectangle(double l, double w) : length(l), width(w) {}
-
-    double area() const override {
-        return length * width;
+class Son : public Father{
+public: 
+    int s_p , t_s_p , ps ;
+    void get_Datason(){
+        cout<<"Enter the Son's Property : "<<endl;
+        cin>>s_p;
+        t_s_p = s_p + pf;
     }
-
-    double perimeter() const override {
-        return 2 * (length + width);
+    void DisplaySon(){
+        cout<<"The Total Son's property is :" <<t_s_p <<endl;
     }
-};
-
-class Circle : public Shape {
-private:
-    double radius;
-
-public:
-    Circle(double r) : radius(r) {}
-
-    double area() const override {
-        return M_PI * radius * radius;
-    }
-
-    double perimeter() const override {
-        return 2 * M_PI * radius;
+    void passmypropertys(){
+        char choice;
+        cout << "Do you want to pass the property to Grandson ? (y/n): ";
+        cin >> choice;
+        if(choice == 'y' || choice == 'Y')
+            ps = t_s_p;
+        else
+            ps = 0;
     }
 };
 
-int main() {
-    Rectangle rect(5, 4);
-    Circle circle(3);
+class Grandson : public Son {
+public: 
+    int g_p,t_g_p;
+    void get_Datagrandson(){
+        cout<<"Enter the Grandson Property : "<<endl;
+        cin>>g_p;
+        t_g_p = g_p + ps;
+    }
+    void DisplayGrandson(){
+        cout<<"The Total Grandson's property is :" <<t_g_p<<endl;
+    }
+};
 
-    cout << "Rectangle:" << endl;
-    cout << "Area: " << rect.area() << endl;
-    cout << "Perimeter: " << rect.perimeter() << endl;
-
-    cout << "\nCircle:" << endl;
-    cout << "Area: " << circle.area() << endl;
-    cout << "Circumference: " << circle.perimeter() << endl;
-
+int main(){
+    Grandson g;
+    g.get_Datafather();
+    g.passmypropertyf();
+    g.get_Datason();
+    g.passmypropertys();
+    g.get_Datagrandson();
+    
+    
+    g.DisplayFather();
+    g.DisplaySon();
+    g.DisplayGrandson();
+    
     return 0;
 }
-
